a76a668cbedd9a567059f9a381ae1915fe54651e
PopovDS617 <popovds.bm@gmail.com>
2022-09-12 22:28:02 +0300

    next-image component settings changed

--- components/events/EventItem.tsx
+++ components/events/EventItem.tsx
@@ -5,7 +5,6 @@ import Button from '../UI/Button';
 import DateIcon from '../UI/Icons/date-icon';
 import ArrowRightIcon from '../UI/Icons/arrow-right-icon';
 import AddressIcon from '../UI/Icons/address-icon';
-import Spinner from '../UI/Spinner';
 
 const EventItem = (props: EventItemType) => {
   const { title, image, date, location, id } = props;
@@ -21,17 +20,24 @@ const EventItem = (props: EventItemType) => {
 
   return (
     <li key={id} className="event-item">
-      <Image
-        src={'/' + image}
-        alt={title}
-        width={800}
-        height={270}
-        objectFit="cover"
-        objectPosition="100% 31%"
-        quality="70"
-        placeholder="blur"
-        blurDataURL={'/' + image}
-      />
+      <div className="event-image-container">
+        <Image
+          src={'/' + image}
+          alt={title}
+          layout="fill"
+          objectFit="cover"
+          // width={900}
+          // height={300}
+          // objectFit="cover"
+          sizes="(max-width: 400px) 300px,
+          (max-width: 768px) 450px,
+          (max-width: 1200px) 900px"
+          objectPosition="100% 29%"
+          quality="70"
+          placeholder="blur"
+          blurDataURL={'/' + image}
+        />
+      </div>
 
       <div className="event-item-content">
         <div className="event-item-summary">
