60f7c087ff2e2d585863632b70c79d2e9b6fa017
PopovDS617 <popovds.bm@gmail.com>
2022-09-21 01:47:28 +0300

    added: search logic and sorting logic

--- components/teams/TeamItem.tsx
+++ components/teams/TeamItem.tsx
@@ -2,16 +2,19 @@ import React, { Fragment } from 'react';
 import { TeamType } from '../../models';
 
 type Props = {
+  place: number;
   name: string;
   games: number;
   points: number;
 };
 
 const TeamItem = (props: Props) => {
-  const { name, games, points } = props;
+  const { name, games, points, place } = props;
   return (
     <Fragment>
-      <div className="rating-team-name">{name}</div>
+      <div className="rating-team-name">
+        {place}. {name}
+      </div>
       <div className="rating-team-games">{games}</div>
       <div className="rating-team-points">{points}</div>
     </Fragment>
